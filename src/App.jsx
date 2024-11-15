4e953fc99e938054ae97928c8f43ae6f59f252fd
Golam Muntakim <seams123456789@gmail.com>
2024-11-14 23:18:48 -0800

    set up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
 const App = () => {
+  useGSAP(()=>{
+    gsap.to(".box", {})
+  })
   return (
    <main>
     <div className="box">
