02841d760559e09f334df0d1c21ce0ac1e7eba73
Golam Muntakim <seams123456789@gmail.com>
2024-11-15 17:38:30 -0800

    use ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
+import { useRef } from "react"
 
 const App = () => {
-  useGSAP(()=>{
-    gsap.to(".box", {})
+  const boxRef = useRef()
+
+useGSAP(()=>{
+  gsap.from(boxRef.current,{
+    x:300,
+    opacity:0,
+    rotate:720,
+    duration:1,
+    delay:1
   })
+})
   return (
    <main>
-    <div className="box">
-
+    <div className="container">
+      <div className="circle"></div>
+      <div ref={boxRef} className="box"></div>
+      </div>
+      <div className="kuch">
+        <div className="circle"></div>
+        <div className="box"></div>
     </div>
    </main>
   )
